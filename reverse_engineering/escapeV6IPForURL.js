cb9caa1720c260de4ae158b70c4d351c08ab8249
Nazar Kovtun <nazar.kovtun@hackolade.com>
2025-05-16 17:57:00 +0300

    HCK-10586: remove template

--- reverse_engineering/escapeV6IPForURL.js
+++ reverse_engineering/escapeV6IPForURL.js
@@ -1,12 +1,3 @@
-/*
- * Copyright © 2016-2025 by IntegrIT S.A. dba Hackolade.  All rights reserved.
- *
- * The copyright to the computer software herein is the property of IntegrIT S.A.
- * The software may be used and/or copied only with the written permission of
- * IntegrIT S.A. or in accordance with the terms and conditions stipulated in
- * the agreement/contract under which the software has been supplied.
- */
-
 const ip = require('ip');
 
 //@see https://en.wikipedia.org/wiki/IPv6_address
